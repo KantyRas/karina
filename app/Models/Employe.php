c85c411c88495244418512385e16a7fff588d745
Kanty <rasolofomananakanty@gmail.com>
2025-09-02 13:24:39 +0300

    modif

--- app/Models/Employe.php
+++ app/Models/Employe.php
@@ -10,7 +10,7 @@ class Employe extends Model
     use HasFactory;
 
     protected $table = 'employes';
-    protected $primaryKey = 'idEmploye';
+    protected $primaryKey = 'idemploye';
     public $timestamps = false;
 
     protected $fillable = [
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     protected $table = 'users';
-    protected $primaryKey = 'idUser';
+    protected $primaryKey = 'iduser';
     public $timestamps = false;
 
     protected $fillable = [
