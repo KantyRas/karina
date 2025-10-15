021350bf297eb9fa8157e6a27a0f8cad8417fb01
Kanty <rasolofomananakanty@gmail.com>
2025-10-15 09:49:13 +0300

    huhhu

--- resources/views/maintenance/carnet/fiche_list_saisie.blade.php
+++ resources/views/maintenance/carnet/fiche_list_saisie.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-body">
                     <form action="{{ route('carnet.ajout_detail_equipement') }}" method="post" class="form-horizontal">
                         @csrf
-    
+
                         <input type="hidden" value={{$historique->idhistoriqueequipement}} name="idhistoriqueequipement">
 
                         @foreach ($parametres as $idfrequence => $items)
@@ -19,17 +19,17 @@
                                 <h4 class="fw-bold mb-3">
                                     {{ $items->first()->frequence->frequence }}
                                 </h4>
-    
+
                                 <div class="row g-3">
                                     @foreach ($items as $index => $parametre)
                                         <div class="col-lg-4 col-md-6 col-sm-12">
                                             <label class="form-label fw-semibold">
                                                 {{ $parametre->nomparametre }}
                                             </label>
-                                            <input 
-                                                type="text" 
-                                                name="param[ {{ $parametre->idparametreequipement }} ]" 
-                                                class="form-control shadow-sm" 
+                                            <input
+                                                type="text"
+                                                name="param[ {{ $parametre->idparametreequipement }} ]"
+                                                class="form-control shadow-sm"
                                                 placeholder="Saisir une valeur..."
                                             >
                                         </div>
@@ -38,9 +38,9 @@
                             </div>
                             <hr>
                         @endforeach
-    
+
                         {{-- <hr class="my-4"> --}}
-    
+
                         <div class="text-end">
                             <button type="submit" class="btn btn-success me-2 px-4">
                                 <i class="fa fa-save me-1"></i> Enregistrer
@@ -58,14 +58,14 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="panel panel-default">
-                
+
                 <div class="panel-heading">
                     <ul class="nav nav-pills ">
                         @foreach ($parametres as $idfrequence => $items)
                         <li>
-                            <a 
-                               class="load-fiche" 
-                               data-historique="{{ $historique->idhistoriqueequipement }}" 
+                            <a
+                               class="load-fiche"
+                               data-historique="{{ $historique->idhistoriqueequipement }}"
                                data-frequence="{{ $items->first()->frequence->idfrequence }}">
                                {{ $items->first()->frequence->frequence }}
                             </a>
@@ -81,33 +81,34 @@
     </div>
     @endsection
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script>
-    $(document).ready(function() {
-            
-        $('.load-fiche').click(function(e){
-            e.preventDefault();
-            console.log("foufoun");
-    
-            var idHistorique = $(this).data('historique');
-            var idFrequence = $(this).data('frequence');
-    
-            // Ajouter la classe active aux onglets
-            $('.nav-pills li').removeClass('active');
-            $(this).parent().addClass('active');
-    
-            $.ajax({
-                url: '/carnets/fiche/saisie/' + idHistorique,
-                type: 'GET',
-                data: { idfrequence: idFrequence },
-                success: function(response){
-                    $('#fiche-container').html(response);
-                },
-                error: function(xhr){
-                    console.error("Erreur AJAX:", xhr);
-                }
+{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>--}}
+@section('scripts')
+    <script>
+        $(document).ready(function() {
+
+            $('.load-fiche').click(function(e){
+                e.preventDefault();
+
+                var idHistorique = $(this).data('historique');
+                var idFrequence = $(this).data('frequence');
+
+                // Ajouter la classe active aux onglets
+                $('.nav-pills li').removeClass('active');
+                $(this).parent().addClass('active');
+
+                $.ajax({
+                    url: '/carnets/fiche/saisie/' + idHistorique,
+                    type: 'GET',
+                    data: { idfrequence: idFrequence },
+                    success: function(response){
+                        $('#fiche-container').html(response);
+                    },
+                    error: function(xhr){
+                        console.error("Erreur AJAX:", xhr);
+                    }
+                });
             });
+
         });
-    
-    });
-</script>
+    </script>
+@endsection
